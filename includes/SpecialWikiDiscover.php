97e6dfb075238df0d55bdf426685d78c5c8300ac
John Lewis <johnflewis93@gmail.com>
2018-05-11 22:19:24 +0100

    Wiki Search: finish logic + show form

--- includes/SpecialWikiDiscover.php
+++ includes/SpecialWikiDiscover.php
@@ -14,25 +14,34 @@ class SpecialWikiDiscover extends SpecialPage {
 
 		$out = $this->getOutput();
 
+		$language = $this->getRequest()->getText( 'language' );
+		$category = $this->getRequest()->getText( 'category' );
+		$languages = Language::fetchLanguageNames( null, 'wmfile' );
+		ksort( $languages );
+		$options = array();
+		foreach ( $languages as $code => $name ) {
+			$options["$code - $name"] = $code;
+		}
+
 		$formDescriptor = [
 			'language' => [
 				'type' => 'select',
 				'name' => 'language',
 				'label-message' => 'wikidiscover-table-language',
 				'options' => $options,
-				'default' => $language,
+				'default' => ( $language ) ? $language : en,
 			],
 			'category' => [
 				'type' => 'select',
 				'name' => 'category',
 				'label-message' => 'wikidiscover-table-category',
 				'options' => $wgCreateWikiCategories,
-				'default' => $category,
+				'default' => ( $category ) ? $category : 'uncategorised',
 			],
 		];
 
 		$htmlForm = HTMLForm::factory( 'ooui', $formDescriptor, $this->getContext() );
-		$htmlForm->setAction( $this->getTitle( 'Special:WikiDiscover' )->getLocalURL() )->setMethod( 'get' )->prepareForm();
+		$htmlForm->setAction( $this->getTitle( 'Special:WikiDiscover' )->getLocalURL() )->setMethod( 'get' )->prepareForm()->show();
 
 		$pager = new WikiDiscoverWikisPager( $wiki, $language, $category );
 		$table = $pager->getBody();

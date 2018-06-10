744eff550657519114667eda2daaa543188d3e9a
paladox <paladox@users.noreply.github.com>
2018-06-11 00:35:34 +0100

    Create WikiDiscover.i18n.magic.php

---
+++ WikiDiscover.i18n.magic.php
@@ -0,0 +1,11 @@
+<?php
+
+$magicWords = [];
+
+$magicWords['en'] = [
+	'numberofwikis' => [ 0, 'numberofwikis' ],
+];
+
+$magicWords['ur'] = [
+	'numberofwikis' => [ 0, 'ویکیوں_کی_تعداد', 'numberofwikis' ],
+];

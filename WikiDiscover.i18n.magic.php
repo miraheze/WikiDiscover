32f1c8066ea22b6ab69d1f58c82b900bfc262707
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-11-21 12:57:01 -0700

    Add more magic words

--- WikiDiscover.i18n.magic.php
+++ WikiDiscover.i18n.magic.php
@@ -5,9 +5,15 @@ $magicWords = [];
 $magicWords['en'] = [
 	'numberofwikis' => [ 0, 'numberofwikis' ],
 	'numberofprivatewikis' => [ 0, 'numberofprivatewikis' ],
+	'numberofpublicwikis' => [ 0, 'numberofpublicwikis' ],
 	'numberofactivewikis' => [ 0, 'numberofactivewikis' ],
+	'numberofinactivewikis' => [ 0, 'numberofinactivewikis' ],
 	'numberofclosedwikis' => [ 0, 'numberofclosedwikis' ],
+	'numberoflockedwikis' => [ 0, 'numberoflockedwikis' ],
+	'numberofdeletedwikis' => [ 0, 'numberofdeletedwikis' ],
+	'numberofinactivityexemptwikis' => [ 0, 'numberofinactivityexemptwikis' ],
 	'numberofwikisincategory' => [ 0, 'numberofwikisincategory' ],
+	'numberofwikisinlanguage' => [ 0, 'numberofwikisinlanguage' ],
 
 ];
 

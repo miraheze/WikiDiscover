17c52142ec67cb7aa02af8a5c9b3e7ec853ed92f
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-02 12:59:50 -0600

    Update SpecialRandomWiki.php

--- includes/SpecialRandomWiki.php
+++ includes/SpecialRandomWiki.php
@@ -1,15 +1,16 @@
 <?php
 
 class SpecialRandomWiki extends SpecialPage {
+	/** @var Config */
 	private $config;
 	
-	function __construct() {
+	public function __construct() {
 		$this->config = $this->getConfig();
 
 		parent::__construct( 'RandomWiki' );
 	}
 
-	function execute( $par ) {
+	public function execute( $par ) {
 		$this->setHeaders();
 		$this->outputHeader();
 
@@ -38,15 +39,15 @@ class SpecialRandomWiki extends SpecialPage {
 
 	}
 
-	function redirectWiki( $formData ) {
+	protected function getGroupName() {
+		return 'wikimanage';
+	}
+
+	private function redirectWiki( $formData ) {
 		$randomwiki = WikiDiscoverRandom::randomWiki( 0, $category = $formData['category'], $formData['language'] );
 
 		header( "Location: https://" . substr( $randomwiki->wiki_dbname, 0, -4 ) . ".{$this->config->get( 'CreateWikiSubdomain' )}/" );
 
 		return true;
-	}
-
-	protected function getGroupName() {
-		return 'wikimanage';
 	}
 }

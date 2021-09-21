e7face4c7ecb14915a696f0abcd525670b09aed4
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-21 16:58:27 -0600

    Update globals.php

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -8,5 +8,8 @@ $wgWikimediaJenkinsCI = true;
 
 require_once( "$IP/extensions/CreateWiki/includes/WikiManager.php" );
 
+$factory = new ConfigFactory();
+$factory->register( 'createwiki', 'GlobalVarConfig::newInstance' );
+
 $wm = new WikiManager( 'wikidb' );
 $wm->create( 'TestWiki', 'en', false, 'uncategorised', 'WikiAdmin', 'WikiAdmin', 'TestWiki' );

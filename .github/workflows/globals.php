d198bb2636ebc8e8bf03bb2eebabf5ae69a327b4
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-21 16:31:54 -0600

    Update globals.php

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -2,5 +2,13 @@
 
 $wgCreateWikiGlobalWiki = 'wikidb';
 $wgCreateWikiDatabase = 'wikidb';
+$wgCreateWikiCacheDirectory = "$IP/cache";
 $wgContinuousIntegrationInstance = true;
 $wgWikimediaJenkinsCI = true;
+
+$wgHooks['SetupAfterCache'][] = 'onSetupAfterCache';
+
+function onSetupAfterCache() {
+	$wm = new WikiManager( 'wikidb' );
+	$wm->create( 'TestWiki', 'en', false, 'uncategorised', 'WikiAdmin', 'WikiAdmin', 'TestWiki' );
+}

94cd3d191f7f321349a93693a128b815c3d8a419
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-21 17:23:58 -0600

    Update globals.php

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -6,16 +6,20 @@ $wgCreateWikiCacheDirectory = "$IP/cache";
 $wgContinuousIntegrationInstance = true;
 $wgWikimediaJenkinsCI = true;
 
-wfGetDB( DB_PRIMARY );
-$dbw->insert(
-	'cw_wikis',
-	[
-		'wiki_dbname' => 'wikidb',
-		'wiki_dbcluster' => 'c1',
-		'wiki_sitename' => 'TestWiki',
-		'wiki_language' => 'en',
-		'wiki_private' => (int)0,
-		'wiki_creation' => 1632266296,
-		'wiki_category' => 'uncategorised'
-	]
-);
+$wgHooks['MediaWikiServices'][] = 'wfOnMediaWikiServices';
+
+function wfOnMediaWikiServices() {
+	wfGetDB( DB_PRIMARY );
+	$dbw->insert(
+		'cw_wikis',
+		[
+			'wiki_dbname' => 'wikidb',
+			'wiki_dbcluster' => 'c1',
+			'wiki_sitename' => 'TestWiki',
+			'wiki_language' => 'en',
+			'wiki_private' => (int)0,
+			'wiki_creation' => 1632266296,
+			'wiki_category' => 'uncategorised'
+		]
+	);
+}

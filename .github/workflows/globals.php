571640eefc2238a79d49e95972c8771a1d1720a7
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-21 19:51:53 -0600

    Update globals.php

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -37,25 +37,29 @@ $wgCreateWikiCacheDirectory = "$IP/cache";
 $wgHooks['MediaWikiServices'][] = 'wfOnMediaWikiServices';
 
 function wfOnMediaWikiServices() {
-	$dbw = wfGetDB( DB_PRIMARY );
+	try {
+		$dbw = wfGetDB( DB_PRIMARY );
 
-	$dbw->insert(
-		'cw_wikis',
-		[
-			'wiki_dbname' => 'wikidb',
-			'wiki_dbcluster' => 'c1',
-			'wiki_sitename' => 'TestWiki',
-			'wiki_language' => 'en',
-			'wiki_private' => (int)0,
-			'wiki_creation' => 1632266296,
-			'wiki_category' => 'uncategorised',
-			'wiki_closed' => (int)0,
-			'wiki_deleted' => (int)0,
-			'wiki_locked' => (int)0,
-			'wiki_inactive' => (int)0,
-			'wiki_inactive_exempt' => (int)0
-		]
-	);
+		$dbw->insert(
+			'cw_wikis',
+			[
+				'wiki_dbname' => 'wikidb',
+				'wiki_dbcluster' => 'c1',
+				'wiki_sitename' => 'TestWiki',
+				'wiki_language' => 'en',
+				'wiki_private' => (int)0,
+				'wiki_creation' => 1632266296,
+				'wiki_category' => 'uncategorised',
+				'wiki_closed' => (int)0,
+				'wiki_deleted' => (int)0,
+				'wiki_locked' => (int)0,
+				'wiki_inactive' => (int)0,
+				'wiki_inactive_exempt' => (int)0
+			]
+		);
+	} catch ( DBQueryError ) {
+		return;
+	}
 }
 
 $wi->readCache();

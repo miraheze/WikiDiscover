cafdb808a3dca5b6a2e99cbd8eab0db76853a6ba
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-23 18:43:08 -0600

    Update globals.php

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -61,12 +61,12 @@ function wfOnMediaWikiServices( MediaWiki\MediaWikiServices $services ) {
 		$oldLoadBalancerFactory = $services->getDBLoadBalancerFactory();
 
 		$services->disableService( 'DBLoadBalancerFactory' );
-		/*$services->redefineService(
+		$services->redefineService(
 			'DBLoadBalancerFactory',
 			static function ( MediaWiki\MediaWikiServices $services ) use ( $oldLoadBalancerFactory ) {
 				return $oldLoadBalancerFactory;
 			}
-		);*/
+		);
 	} catch ( Wikimedia\Rdbms\DBQueryError $e ) {
 		return;
 	}

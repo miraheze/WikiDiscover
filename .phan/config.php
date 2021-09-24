4fe2089496592cc505bebbd1f90fa28b7fb73d9f
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-23 18:04:33 -0600

    Update workflow (#63)

--- .phan/config.php
+++ .phan/config.php
@@ -2,23 +2,19 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
-$cfg['target_php_version'] = '7.3';
-
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'], [
-		'mediawiki',
+		'../../extensions/CreateWiki',
 	]
 );
 
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'], [
-		'mediawiki',
+		'../../extensions/CreateWiki',
 	]
 );
 
-$cfg['suppress_issue_types'] = array_merge(
-	$cfg['suppress_issue_types'], []
-);
+$cfg['suppress_issue_types'] = [];
 
 $cfg['scalar_implicit_cast'] = true;
 

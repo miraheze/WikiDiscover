c72a3fc95bf39f56a9765c1e37b261a9fe251bcc
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-01-20 13:30:34 -0700

    Create ApiQueryWikiDiscoverTest.php

---
+++ tests/phpunit/ApiQueryWikiDiscoverTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @group Wikidiscover
+ * @group Database
+ * @group medium
+ * @coversDefaultClass ApiQueryWikiDiscover
+ */
+class ApiQueryWikiDiscoverTest extends ApiTestCase {
+
+	/**
+	 * @covers ::__construct
+	 * @covers ::execute
+	 * @covers ::run
+	 */
+	public function testQueryWikiDiscover() {
+		$data = $this->doApiRequest(
+			[
+				'action' => 'query',
+				'list' => 'wikidiscover',
+				'state' => 'active',
+				'siteprop' => 'url|sitename'
+			]
+		);
+
+		$this->assertEquals( 'query', $data[0]['query'] );
+		$this->assertArrayHasKey( 'wikidiscover', $data[0]['query'] );
+		$this->assertGreaterThan( 0, count( $data[0]['query']['wikidiscover'] ) );
+		foreach ( $data[0]['query']['wikidiscover'] as $wiki ) {
+			$this->assertArrayHasKey( 'url', $wiki );
+			$this->assertArrayHasKey( 'sitename', $wiki );
+		}
+	}
+}

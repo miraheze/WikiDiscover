74f8f44e69c6ea461818fa49652d1f4f00befa82
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-11 14:42:17 -0700

    Fix

--- includes/WikiDiscoverWikisPager.php
+++ includes/WikiDiscoverWikisPager.php
@@ -7,6 +7,7 @@ use MediaWiki\Context\RequestContext;
 use MediaWiki\Html\Html;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Pager\TablePager;
+use MediaWiki\SpecialPage\SpecialPage;
 use Miraheze\ManageWiki\Helpers\ManageWikiSettings;
 use Wikimedia\Rdbms\IReadableDatabase;
 

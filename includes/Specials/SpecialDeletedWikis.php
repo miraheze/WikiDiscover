705b5e6711cd1ada94ded4f8c3e1b2c802c83cf5
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-16 18:48:42 -0700

    Fix

--- includes/Specials/SpecialDeletedWikis.php
+++ includes/Specials/SpecialDeletedWikis.php
@@ -11,7 +11,10 @@ class SpecialDeletedWikis extends SpecialPage {
 		parent::__construct( 'DeletedWikis' );
 	}
 
-	public function execute( $par ) {
+	/**
+	 * @param ?string $par
+	 */
+	public function execute( $par ): void {
 		$this->setHeaders();
 		$this->outputHeader();
 

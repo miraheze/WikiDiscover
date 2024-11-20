09e7e487d4379c8f53e50669cf3574550f33742f
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-20 13:32:51 -0700

    Remove

--- includes/Specials/SpecialDeletedWikis.php
+++ includes/Specials/SpecialDeletedWikis.php
@@ -24,7 +24,6 @@ class SpecialDeletedWikis extends SpecialPage {
 		$this->outputHeader();
 
 		$pager = new DeletedWikisPager(
-			$this->getConfig(),
 			$this->getContext(),
 			$this->connectionProvider,
 			$this->getLinkRenderer()

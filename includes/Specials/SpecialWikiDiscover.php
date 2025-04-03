f14c6705b7b95f0ecdd7e49f44f7f2fd32537b83
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-02 19:33:17 -0600

    Update

--- includes/Specials/SpecialWikiDiscover.php
+++ includes/Specials/SpecialWikiDiscover.php
@@ -31,19 +31,19 @@ class SpecialWikiDiscover extends SpecialPage {
 		$state = $this->getRequest()->getText( 'state' );
 
 		$stateOptions = [
-			'(any)' => 'any',
-			'Active' => 'active',
-			'Locked' => 'locked',
+			'wikidiscover-label-any' => 'any',
+			'wikidiscover-label-active' => 'active',
+			'wikidiscover-label-locked' => 'locked',
 		];
 
 		if ( $this->getConfig()->get( 'CreateWikiUseClosedWikis' ) ) {
-			$stateOptions['Closed'] = 'closed';
+			$stateOptions['wikidiscover-label-closed'] = 'closed';
 		}
 
-		$stateOptions['Deleted'] = 'deleted';
+		$stateOptions['wikidiscover-label-deleted'] = 'deleted';
 
 		if ( $this->getConfig()->get( 'CreateWikiUseInactiveWikis' ) ) {
-			$stateOptions['Inactive'] = 'inactive';
+			$stateOptions['wikidiscover-label-inactive'] = 'inactive';
 		}
 
 		$formDescriptor = [
@@ -53,7 +53,11 @@ class SpecialWikiDiscover extends SpecialPage {
 			],
 			'language' => [
 				'type' => 'language',
-				'options' => [ '(any)' => 'any' ],
+				'options' => [
+					// We cannot use options-messages here as otherwise
+					// it overrides all language options.
+					$this->msg( 'wikidiscover-label-any' )->text() => 'any',
+				],
 				'name' => 'language',
 				'label-message' => 'wikidiscover-table-language',
 				'default' => $language ?: 'any',
@@ -62,14 +66,16 @@ class SpecialWikiDiscover extends SpecialPage {
 				'type' => 'select',
 				'name' => 'category',
 				'label-message' => 'wikidiscover-table-category',
-				'options' => [ '(any)' => 'any' ] + $this->getConfig()->get( 'CreateWikiCategories' ),
+				'options' => [
+					$this->msg( 'wikidiscover-label-any' )->text() => 'any',
+				] + $this->getConfig()->get( 'CreateWikiCategories' ),
 				'default' => $category ?: 'any',
 			],
 			'state' => [
 				'type' => 'select',
 				'name' => 'state',
 				'label-message' => 'wikidiscover-table-state',
-				'options' => $stateOptions,
+				'options-messages' => $stateOptions,
 				'default' => $state ?: 'any',
 			],
 		];
@@ -83,10 +89,10 @@ class SpecialWikiDiscover extends SpecialPage {
 				'type' => 'select',
 				'name' => 'visibility',
 				'label-message' => 'wikidiscover-table-visibility',
-				'options' => [
-					'(any)' => 'any',
-					'Public' => 'public',
-					'Private' => 'private',
+				'options-messages' => [
+					'wikidiscover-label-any' => 'any',
+					'wikidiscover-label-public' => 'public',
+					'wikidiscover-label-private' => 'private',
 				],
 				'default' => $visibility ?: 'any',
 			];
